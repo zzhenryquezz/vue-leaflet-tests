cf9e766daa2168142fd8a2494dc410f0d48efbc0
Henrique <henriqueoliwork@gmail.com>
2021-01-10 01:04:38 -0300

    github: add async-loop to deploy script

--- vue-2-pixioverlay-async-loop-and-chunks/vue.config.js
+++ vue-2-pixioverlay-async-loop-and-chunks/vue.config.js
@@ -2,5 +2,5 @@ const path = require('path');
 
 module.exports = {
     outputDir: path.resolve(__dirname, "../site/async-loop-and-chunks"),
-    publicPath: process.env.NODE_ENV === 'production' ? '/async-loop-and-chunks' : '/'
+    publicPath: process.env.NODE_ENV === 'production' ? '/vue-leaflet-tests/async-loop-and-chunks' : '/'
 }
